fe4267426a3cef491f908b3aca07a730d65aca14
Sriraam-K <116096802+Sriraam-K@users.noreply.github.com>
2023-10-04 23:47:43 +0530

    Add files via upload

---
+++ Web Development/Task - 2 Sriraam K - Portfolio/assets/js/main.js
@@ -0,0 +1,80 @@
+/*  SHOW MENU  */
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+/*  MENU SHOW  */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*  MENU HIDDEN  */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*  REMOVE MENU MOBILE  */
+const navLink = document.querySelectorAll('.nav_link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*  ADD BLUR TO HEADER  */
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+/*  SHOW SCROLL UP  */ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*  SCROLL SECTIONS ACTIVE LINK  */
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+/*  SCROLL REVEAL ANIMATION  */
+const sr = ScrollReveal({
+	origin: 'top',
+    distance: '60px',
+    duration: 3000,
+	delay: 400,
+    // reset: true
+})
+
+sr.reveal('.home_data, .home_social, .contact_container, .footer_container')
+sr.reveal('.home_image', {origin: 'bottom'})
+sr.reveal('.services_card, .projects_card', {interval: 100})
+sr.reveal('.about_data, .skills_data, .education_data', {origin: 'left'})
+sr.reveal('.about_image, .skills_content, .education_content', {origin: 'right'})
